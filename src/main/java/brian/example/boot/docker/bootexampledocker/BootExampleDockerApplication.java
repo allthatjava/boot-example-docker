32ae2cc9170477067be0d9696824adeaf3df2901
allthatjava <allthatjava@yahoo.ca>
2024-04-17 13:03:42 -0400

    Adds name parameter

--- src/main/java/brian/example/boot/docker/bootexampledocker/BootExampleDockerApplication.java
+++ src/main/java/brian/example/boot/docker/bootexampledocker/BootExampleDockerApplication.java
@@ -3,6 +3,7 @@ package brian.example.boot.docker.bootexampledocker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BootExampleDockerApplication {
 
     @GetMapping("/message")
-    public String getMessages(@RequestParam("name") String name){
+    public String getMessages(@Nullable @RequestParam("name") String name){
         log.debug("Welcome to dockerize example:"+name);
-        return "Welcome to dockerize example 2";
+        return "Welcome to dockerize example : "+name;
     }
 
     public static void main(String[] args) {
